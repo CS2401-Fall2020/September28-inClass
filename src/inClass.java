f7ae45fef3d6b386098b90700f2f5cd98a72286c
Dan DeBlasio <dan@dandeblasio.com>
2020-09-28 11:48:27 -0600

    coding myQuickSort

--- src/inClass.java
+++ src/inClass.java
@@ -2,8 +2,65 @@
 public class inClass {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    char[] test1 = {'c','f','q','a','g','f','b'};
+    char[] test1correct = {'a','b','c','f','f','g','q'};
+    myQuickSort(test1, 0, test1.length-1);
+    for(int i=0; i<test1.length; i++) {
+      System.out.println("Index " + i + " should be " + test1correct[i] + " and its " + test1[i] );
+      assert(test1correct[i] == test1[i] );
+    }
 
+  }
+  
+  public static void myQuickSort(char[] charArray, int start, int end) {
+    // base case
+    if(start >= end) {
+      return;
+    }
+    
+    
+    // recursive case
+    
+    //finds pivot
+    int pivot_index = start + (end-start)/2;
+    char pivot = charArray[pivot_index];
+    
+    //shuffle around pivot
+    int l = start;
+    int h = end;
+    boolean done = false;
+    
+    /*
+     * 
+     *  ---------------------
+     *  |   |   | p |   |   |
+     *  ---------------------
+     *    l               h 
+     * 
+     */
+    while(!done) {
+      while(charArray[l] < pivot)
+        l++;
+      
+      while(charArray[h] > pivot)
+        h--;
+      
+      if(l >= h)
+        done = true;
+      else {
+        char temp = charArray[l];
+        charArray[l] = charArray[h];
+        charArray[h] = temp;
+        l++;
+        h--;
+      }
+    }
+    
+    
+    // everything to the left of (including) h is smaller (or equal) than the pivot
+    myQuickSort(charArray, start, h);
+    // everything to the right (not including) h is larger (or equal) than the pivot
+    myQuickSort(charArray, h+1, end);
   }
 
 }
